6e6e976c00db6533c25ea4dfb3e9ceec1819ce4c
AMJ <isaelwebdeveloper@gmail.com>
2020-03-03 18:46:59 -0700

    created card for person

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Jumbotron from './components/Jumbotron/Jumbotron';
-import Me from './components/Me/Me';
+import Card from './components/Card/Card';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Jumbotron />
-      <Me />
+      <Card />
     </div>  
   );
 }
