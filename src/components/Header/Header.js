212f95d4adc1051b35750334a25bd5878b99781c
AMJ <isaelwebdeveloper@gmail.com>
2020-03-04 21:06:11 -0700

    added header with styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import './Header.css'
 
-export default class Header extends React.Component{
-    render(){
-        return (
-            <nav className='header'>
-                <div className='logo'>Logo</div>
-                <nav className='nav-button-container'>
-                    <div className='nav-button'>Houses</div>
-                    {/* <div className='nav-button'></div> */}
-                    <div className='nav-button'>About</div>
-                    <div className='nav-button'>Contact</div>
-                </nav>
-            </nav>
-        )
-    }
+export default function Header () {
+    return (
+        <div className='header-container'>
+            <div className='relative'>
+                <div className='logo-container'>
+                    <div className='outer-box relative'>
+                        <p className='n'>
+                            N
+                        </p>
+                        <div className='line'></div>
+                        <div className='bottom-line'></div>
+                        <p className='v'>
+                            V
+                        </p>
+                    </div>
+                </div>
+
+                <div className='nav container borderXwidth'>
+                    <a>Houses</a> 
+                    <a>Contact</a> 
+                    <a>About</a> 
+                </div>
+            </div>
+        </div>
+    )
 }
