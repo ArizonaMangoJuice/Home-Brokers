4a20f21f2a45e8b6f4a0fc203950987a30467fad
AMJ <isaelwebdeveloper@gmail.com>
2020-03-09 16:05:24 -0600

    added place holder text for buyers

--- src/components/BuyerJumbo/BuyerJumbo.js
+++ src/components/BuyerJumbo/BuyerJumbo.js
@@ -21,6 +21,17 @@ export default function BuyerJumbo(){
                         <i className="current-page fas fa-angle-double-right"></i>
                         <span className='current-page'>Buyers</span>
                     </nav>
+                    <div className='title-and-info readable-color'>
+                        <div className='page-info-container'>
+                            <p className='page-info-title'>BUYERS</p>
+                            <p className='page-info-long-title'>HIGH-QUALITY BUYER SERVICES</p>
+                            <p className='page-info-block'>“MY COMPREHENSIVE, HIGH-QUALITY SERVICES CAN SAVE YOU TIME AND MONEY, AS WELL AS MAKE THE EXPERIENCE MORE ENJOYABLE AND LESS STRESSFUL.”</p>
+                        </div>
+                        <div className='page-info-long-info'>
+                            <p>Whether you’re looking for your first home, your dream home or an investment property, I’m committed to providing you with the highest quality buyer service. When making the type of investment that buying a home requires, you deserve to have all the latest market information and the very best advice, both of which I can provide. Buying a home is an exciting and complex adventure. It can also be a very time-consuming and costly one if you’re not familiar with all aspects of the process, and don’t have all the best information and resources at hand.</p>
+                            <p>One of my specialties is representing the best interests of Atlanta area buyers throughout the home buying process. My comprehensive, high-quality services can save you time and money, as well as make the experience more enjoyable and less stressful. If you’re like most people, buying a home is the biggest investment you will ever make. So whether you’re buying a starter home, your dream home or an investment property, why not take advantage of my experience as a local market expert for Atlanta to make the most informed decisions you can, every step of the way?</p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className='mch for-buyers-color relative'>
