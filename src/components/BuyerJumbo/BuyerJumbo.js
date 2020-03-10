81796161078e2a0aee5bb91965f163e6b1ab3417
AMJ <isaelwebdeveloper@gmail.com>
2020-03-09 18:57:34 -0600

    added small image links and component

--- src/components/BuyerJumbo/BuyerJumbo.js
+++ src/components/BuyerJumbo/BuyerJumbo.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './BuyerJumbo.css'
 
 import {Link} from 'react-router-dom'
+import ImageLink from '../ImageLink/ImageLink'
 
 export default function BuyerJumbo(){
     return (
@@ -34,17 +35,18 @@ export default function BuyerJumbo(){
                     </div>
                 </div>
             </div>
-            <div className='mch for-buyers-color relative'>
-                <div className='container-b-info'>
-                        <div className='title-and-info readable-color'>
+            <div className='mch for-buyers-color alternate-background relative'>
+                <div className='container-b-info ' >
+                        <div className='title-and-info '>
                             <div className='page-info-container'>
                                 <p className='page-info-title relative bb'>WOULD YOU LIKE TO</p>
                                 <p className='page-info-long-title'>LEARN MORE ABOUT THE PROCESS?</p>
                                 <p className='page-info-block'>THIS DETAILED HOME BUYING GUIDE WILL ADDRESS A FEW OF THE KEY QUESTIONS THAT MOST BUYERS HAVE.</p>
                             </div>
                             <div className='page-info-long-info'>
-                                <p>Whether you’re looking for your first home, your dream home or an investment property, I’m committed to providing you with the highest quality buyer service. When making the type of investment that buying a home requires, you deserve to have all the latest market information and the very best advice, both of which I can provide. Buying a home is an exciting and complex adventure. It can also be a very time-consuming and costly one if you’re not familiar with all aspects of the process, and don’t have all the best information and resources at hand.</p>
-                                <p>One of my specialties is representing the best interests of Atlanta area buyers throughout the home buying process. My comprehensive, high-quality services can save you time and money, as well as make the experience more enjoyable and less stressful. If you’re like most people, buying a home is the biggest investment you will ever make. So whether you’re buying a starter home, your dream home or an investment property, why not take advantage of my experience as a local market expert for Atlanta to make the most informed decisions you can, every step of the way?</p>
+                                <ImageLink title="FINANCIAL OBLIGATIONS" imgLink='hugging.jpg'/>
+                                <ImageLink title="A ROAD MAP OF THE ENTIRE PROCESS" imgLink='denver.jpg'/>
+                                <ImageLink title="WHY YOU SHOULD HIRE A REAL ESTATE PROFESSIONAL" imgLink='family.jpg'/>
                             </div>
                         </div>
                 </div>
---
+++ src/components/ImageLink/ImageLink.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './ImageLink.css'
+import {
+    Link
+} from 'react-router-dom'
+
+
+export default class ImageLink extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            hovered: false
+        }
+    }
+
+    darkScreen() {
+        this.setState({hovered: !this.state.hovered})
+    } 
+
+    render(){
+        return (
+            <Link onMouseEnter={() => this.darkScreen()} 
+                    onMouseLeave={() => this.darkScreen()} 
+                    to='/buyers'
+                    className='a-format' href='#'>                   
+                    <div className='small-picture-link relative'>
+                        <div className={this.state.hovered ? 'dark' : ''}></div>
+                        <img className={this.state.hovered ? 'small-link-picture scale' : 'small-link-picture'} src={require('../../images/' + this.props.imgLink)} />
+                        <p className='small-link-title'>{this.props.title}</p>
+                    </div>
+            </Link>
+        )
+    }
+}
