e2c4b5533f89a770ee964a7321181f1496c14f7f
AMJ <isaelwebdeveloper@gmail.com>
2020-03-10 15:58:58 -0600

    added small version of component links

--- src/components/HomeValuation/HomeValuation.js
+++ src/components/HomeValuation/HomeValuation.js
@@ -3,7 +3,7 @@ import './HomeValuation.css'
 import {
     Link
 } from 'react-router-dom'
-
+import SmallImageLink from '../SmallImageLink/SmallImageLink'
 export default class HomeValuation extends React.Component{
     constructor(props){
         super(props)
@@ -16,10 +16,43 @@ export default class HomeValuation extends React.Component{
         return (
             <div>
                 <div>
-                    <div className='mch-md relative home-val-img'>
-
-                        <h1 className='title-page'>Home Valuation</h1>
+                    <div className='mch-md relative home-val-img mch-md-img'>
+                        <div className='dark-blue-glass'></div>
+                        <h1 className='title-page raised'>Home Valuation</h1>
                     </div>                    
+                </div>
+
+                <div className='grid-container'>
+                    <div className='mch  relative '>
+                        <div className='dark-blue-glass'></div>
+                        
+                        <div className='container-b-info'>
+                            <nav className='buyer-links'>
+                                <Link className='back-page' to='/'>
+                                    <span className='no-margin'>Home</span>
+                                </Link>
+                                <i className="current-page fas fa-angle-double-right"></i>
+                                <span className='current-page'>Buyers</span>
+                            </nav>
+                            <div className='mch relative'>
+                                <div className='container-b-info-md' >
+                                        <div className='title-and-info-sm '>
+                                            <div className='page-info-container'>
+                                                <p className='page-info-block'>I know that selling your home is a major life event, and in today’s market, is a multifaceted effort. That’s why I make it easy for my clients to count on me to manage the entire process for them. My unparalleled marketing strategies will maximize exposure and help you sell your home for the highest possible price in the shortest amount of time. I am dedicated to providing you great service and the least amount of stress.</p>
+                                                <p className='page-info-block'>An automatic home valuation tool uses proprietary and public sales data, along with market trends to provide you with an estimate of what your home may be worth today. Click the link below. In order to fully asses your property value I will need to schedule a custom marketing appointment.</p>
+                                                <p className='page-info-block'>Click the button for a home valuation:</p>
+                                            </div>
+                                            <div className='page-info-long-info'>
+                                                <SmallImageLink title="FINANCIAL OBLIGATIONS" imgLink='hugging.jpg'/>
+                                                <SmallImageLink title="A ROAD MAP OF THE ENTIRE PROCESS" imgLink='denver.jpg'/>
+                                                <SmallImageLink title="WHY YOU SHOULD HIRE A REAL ESTATE PROFESSIONAL" imgLink='family.jpg'/>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         )
---
+++ src/components/SmallImageLink/SmallImageLink.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './SmallImageLink.css'
+import {
+    Link
+} from 'react-router-dom'
+
+
+export default class SmallImageLink extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            hovered: false
+        }
+    }
+
+    darkScreen() {
+        this.setState({hovered: !this.state.hovered})
+    } 
+
+    render(){
+        return (
+            <Link onMouseEnter={() => this.darkScreen()} 
+                    onMouseLeave={() => this.darkScreen()} 
+                    to='/buyers'
+                    className='a-format' href='#'>                   
+                    <div className='smaller-picture-link relative'>
+                        <div className={this.state.hovered ? 'dark' : ''}></div>
+                        <img className={this.state.hovered ? 'smaller-link-picture scale' : 'smaller-link-picture'} src={require('../../images/' + this.props.imgLink)} />
+                        <p className='smaller-link-title'>{this.props.title}</p>
+                    </div>
+            </Link>
+        )
+    }
+}
