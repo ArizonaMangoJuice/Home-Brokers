13d2f2211433a4c9b53cc4cfcc616fc33ede2ebb
AMJ <isaelwebdeveloper@gmail.com>
2020-03-14 15:13:57 -0600

    created phone and ipad css for seller

--- src/components/Sellers/Seller.js
+++ src/components/Sellers/Seller.js
@@ -19,7 +19,7 @@ export default class Seller extends React.Component{
             <div>
                 <div className='mch-md relative list-with-us-img mch-md-img'>
                     <div className='dark-blue-glass'></div>
-                    <h1 className='title-page raised'>For Sellers</h1>
+                    <h1 className='title-page raised long-title-phone'>For Sellers</h1>
                 </div>  
 
                 <div className='grid-container'>
@@ -38,13 +38,15 @@ export default class Seller extends React.Component{
                                 <div className='container-b-info-md' >
                                         <div className='title-and-info-sm block '>
                                             <div className='page-info-container'>
-                                                <p className='page-info-long-title seller-title nolm'>COMPREHENSIVE LISTING SERVICES</p>
-                                                <p className='page-info-block seller-quote nolm'>“WHATEVER YOUR REASONS ARE FOR SELLING, MY PROVEN, EFFECTIVE MARKETING STRATEGIES WILL ENSURE THAT YOU GET THE HIGHEST POSSIBLE PRICE FOR YOUR HOME.”</p>
-                                                <p className='page-info-block nolm'>The decision to sell a home is often dependent on many factors. Whatever your reasons are for selling, my proven effective marketing strategies will ensure that you get the highest possible price for your home. I guarantee to provide you with professional, ethical and confidential service and keep you fully informed every step of the way. For most families, their home is their largest financial asset, and deciding to sell it is a big decision that involves a lot of preparation and work. When you’re ready to sell it’s important to have an experienced real estate professional handle the details involved in the successful sale of a home for top dollar.</p>
-                                                <p className='page-info-block nolm'>As an experienced professional who has helped many Atlanta residents sell their homes, I know how to handle every aspect of the sales process – from strategically marketing and showcasing your home to making sure everything’s signed, sealed and delivered by the closing date.</p>
-                                                <p className='page-info-block nolm'>Providing you with comprehensive, high-quality listing service is my top priority. So when you decide to sell your home, please contact me and let’s get started!</p>
-                                                <p className='page-info-block nolm'>CLICK BUTTON BELOW TO FIND OUT WHAT YOUR HOME IS WORTH!</p>
-                                                <FancyLink text='WHAT’S MY HOME WORTH?'/>
+                                                <p className='page-info-long-title seller-title nolm  '>COMPREHENSIVE LISTING SERVICES</p>
+                                                <p className='page-info-block seller-quote nolm '>“WHATEVER YOUR REASONS ARE FOR SELLING, MY PROVEN, EFFECTIVE MARKETING STRATEGIES WILL ENSURE THAT YOU GET THE HIGHEST POSSIBLE PRICE FOR YOUR HOME.”</p>
+                                                <p className='page-info-block nolm ipad-margin'>The decision to sell a home is often dependent on many factors. Whatever your reasons are for selling, my proven effective marketing strategies will ensure that you get the highest possible price for your home. I guarantee to provide you with professional, ethical and confidential service and keep you fully informed every step of the way. For most families, their home is their largest financial asset, and deciding to sell it is a big decision that involves a lot of preparation and work. When you’re ready to sell it’s important to have an experienced real estate professional handle the details involved in the successful sale of a home for top dollar.</p>
+                                                <p className='page-info-block nolm ipad-margin'>As an experienced professional who has helped many Atlanta residents sell their homes, I know how to handle every aspect of the sales process – from strategically marketing and showcasing your home to making sure everything’s signed, sealed and delivered by the closing date.</p>
+                                                <p className='page-info-block nolm ipad-margin'>Providing you with comprehensive, high-quality listing service is my top priority. So when you decide to sell your home, please contact me and let’s get started!</p>
+                                                <p className='page-info-block nolm ipad-margin'>CLICK BUTTON BELOW TO FIND OUT WHAT YOUR HOME IS WORTH!</p>
+                                                <div className='ipad-margin'>
+                                                    <FancyLink text='WHAT’S MY HOME WORTH?'/>
+                                                </div>
                                             </div>
                                         </div>
                                 </div>
