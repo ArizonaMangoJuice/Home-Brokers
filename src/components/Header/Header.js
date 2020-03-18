11f426913397354d20fbe25f7bc8f39796a87725
AMJ <isaellizama@gmail.com>
2020-03-18 11:24:28 -0600

    added functionality to mobile menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,43 @@ import React from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
 
-export default function Header () {
+export default class Header extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            menu: false
+        }
+    }
+
+    mobile = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
+    render(){
     return (
-        <div className='header-container'>
+        <div className='header-container relative'>
+            <div className={this.state.menu ? 'mobile-menu' : 'hidden'} >
+                <div className='mobile-links'>
+                    <nav className='burger-links-nav'>
+                        <Link className='burger-links' to='/contact'>
+                            Houses
+                        </Link>
+                        
+                        <Link className='burger-links' to='/about'>
+                            About
+                        </Link>
+
+                        <Link className='burger-links' to='/contact'>
+                            Contact Us
+                        </Link>
+                    </nav>
+                </div>
+                <button onClick={() => this.mobile()} className='mobile-hidden-button'>
+
+                </button>
+            </div>
             <div className='relative'>
                 <div className='logo-container'>
                     <Link to='/' className='logo-link'>
@@ -37,11 +71,11 @@ export default function Header () {
                 </div>
 
                 <nav className='phone-nav'>
-                    <div className='burger'>
+                    <button onClick={() => this.mobile()} className='burger'>
                         <div className='burger-line'></div>
                         <div className='burger-line'></div>
                         <div className='burger-line'></div>
-                    </div>
+                    </button>
                     <div className='phone-phone'>
                         <a className="phone-header-link" href="tel:303-350-6089"><i className="fas fa-phone-square-alt "></i></a>
                     </div>
@@ -53,4 +87,5 @@ export default function Header () {
             </div>
         </div>
     )
+}
 }
